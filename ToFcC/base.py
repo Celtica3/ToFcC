b7c716b4b4708fe35c1a332b81372d8fdbe0568e
Celtica3 <96926560+Celtica3@users.noreply.github.com>
2023-01-21 13:14:53 -0500

    ref: skip logs

--- ToFcC/base.py
+++ ToFcC/base.py
@@ -13,6 +13,8 @@ def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
         for file in files:
+            if file.endswith("log"):
+                continue
             ziph.write(os.path.join(root, file), 
                        os.path.relpath(os.path.join(root, file), 
                                        os.path.join(path, '..')))
